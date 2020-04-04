45a4867308af4e831c4a1ddca5f9e2e66caa6c87
KaroDev3 <karodev3@gmail.com>
2020-04-04 16:28:21 +0000

    Adding add_node_end and print_list functions into --useful_func.c

--- mini_shell/shell.h
+++ mini_shell/shell.h
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
 #include <sys/stat.h>
-#define TOK_DELIM " \t\r\n\a"
+#define TOK_DELIM " \t\r\n"
+
+/**
+ * struct list_p - singly linked list
+ * @dir: string - (malloc'ed string)
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for directories of PATH
+ */
 typedef struct list_path
 {
         char *dir;
         struct list_path *next;
 } list_p;
-int _strlen(char *s);int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
+
 void execute_line(char **arg);
 char *read_line(void);
 char **split_line(char *line);
 char *_getenv(char *name);
+
+/**
+ * useful_func
+ */
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 list_p *add_node_end(list_p **head, const char *str);
 size_t print_list(const list_p *h);
--- mini_shell/useful_func.c
+++ mini_shell/useful_func.c
@@ -1,10 +1,10 @@
 #include "shell.h"
+
 /**
  * _strlen - return length of a string
  * @s: char type
  * Return:  length of string
  */
-
 int _strlen(char *s)
 {
 	int len;
@@ -14,14 +14,13 @@ int _strlen(char *s)
 		len++;
 	return (len);
 }
-#include "shell.h"
+
 /**
  *_strcpy - make
  *@dest: make
  *@src: make
  * Return: return the value dest
  */
-
 char *_strcpy(char *dest, char *src)
 {
 	int cont;
@@ -34,3 +33,52 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 
+/**
+ *add_node_end - append node to list pointed
+ *@head: pointer to pointer
+ *@str: string to initialize new node
+ *Return: new or NULL
+ */
+list_p *add_node_end(list_p **head, const char *dir)
+{
+        list_p *last = *head;
+        list_p *new = malloc(sizeof(list_p));
+
+        if (new == NULL)
+                return (NULL);
+        new->next = NULL;
+        new->dir = strdup(dir);
+        if (last)
+        {
+                while (last->next)
+                        last = last->next;
+                last->next = new;
+        }
+        else
+                *head = new;
+        return (new);
+}
+
+/**
+ * print_list - prints all the elements of a list_p list.
+ * If str is NULL, print [0] (nil).
+ * @h: pointer to list_p element.
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_p *h)
+{
+	unsigned int index = 0;
+
+	while (h != NULL)
+	{
+		if (!h->dir)
+			printf("[0] (nil)\n");
+		else
+			printf("%s\n", h->dir);
+
+		h = h->next;
+		index++;
+	}
+
+	return (index);
+}
