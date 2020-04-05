b807bcc9ee76466ddb7d47eb531725f3d088c62f
KaroDev3 <karodev3@gmail.com>
2020-04-05 15:17:04 +0000

    Handle the end of file condition (Ctrl+D) --shell.c

--- mini_shell/shell.c
+++ mini_shell/shell.c
@@ -1,13 +1,23 @@
 #include "shell.h"
 int main(void)
 {
-	char *line;
+	char *line = NULL;
 	char **arg;
+	size_t bufsize = 0;
 
-	line = read_line();
-	arg = split_line(line);
-	execute_line(arg);
-	free (line);
-	free(arg);
+	while (1)
+	{
+		printf("#cisfun$ ");
+		if (getline(&line, &bufsize, stdin) == -1)
+		{
+			write(1, "\n", 1);
+			break;
+		}
+		arg = split_line(line);
+		execute_line(arg);
+		free (line);
+		free(arg);
+	}
+
 	return (0);
 }
