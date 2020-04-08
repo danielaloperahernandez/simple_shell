68de79a24d5264493ba7315dd5d46a4d841ded7d
KaroDev3 <karodev3@gmail.com>
2020-04-08 21:24:26 +0000

    Fix case only 'spaces' and Adding non-interactive mode

--- shell.c
+++ shell.c
@@ -4,19 +4,29 @@ int main(void)
 	char *line = NULL;
 	char **arg;
 	size_t bufsize = 0;
+	int line_len = 0;
 
 	while (1)
 	{
-		printf("#cisfun$ ");
-		if (getline(&line, &bufsize, stdin) == -1)
+		if (isatty(fileno(stdin)))
+			write(1, "#cisfun$ ", 9);
+		line_len = getline(&line, &bufsize, stdin);
+		if (line_len == -1 && isatty(fileno(stdin)))
 		{
 			write(1, "\n", 1);
+			free(line);
+			exit(0);
+		}
+                if (line_len == -1 && !isatty(fileno(stdin)))
+		{
 			free(line);
 			exit(0);
 		}
 		if (_strcmp(line, "\n") == 0)
 			continue;
 		arg = split_line(line);
+		if (!*arg)
+			continue;
 		if (_strcmp("exit", *arg) == 0)
 			built_exit(line, arg);
 		else if (_strcmp("env", *arg) == 0)
